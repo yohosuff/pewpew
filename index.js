5206b7f301d209e74f3c5f49a96cf917c911009b
Joey Goertzen <joey.goertzen@gmail.com>
2021-05-17 23:53:13 -0600

    add camera

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const player = {
-    width: 50,
-    height: 50,
-    speed: 100,
-    position: {
-        x: 0,
-        y: 0,
-    },
-    velocity: {
-        x: 0,
-        y: 0,
-    }
-};
-
-const input = {
-    up: false,
-    down: false,
-    left: false,
-    right: false,
-};
-
 function update(progress) {
     if (input.up) {
         player.velocity.y = -player.speed;
@@ -39,17 +18,24 @@ function update(progress) {
     const delta = progress / 1000;
     player.position.x += player.velocity.x * delta;
     player.position.y += player.velocity.y * delta;
-}
 
-const canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const context = canvas.getContext('2d');
-context.fillStyle = 'red';
+    follow(camera, player);
+}
 
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillRect(player.position.x, player.position.y, player.width, player.height);
+
+    const drawables = [player, wall];
+
+    drawables.forEach(drawable => {
+        context.fillStyle = drawable.color;
+        context.fillRect(
+            drawable.position.x - (camera.position.x - window.innerWidth / 2),
+            drawable.position.y - (camera.position.y - window.innerHeight / 2),
+            drawable.width,
+            drawable.height
+        );
+    });
 }
 
 function loop(timestamp) {
@@ -59,16 +45,6 @@ function loop(timestamp) {
     lastRender = timestamp;
     window.requestAnimationFrame(loop);
 }
-
-let lastRender = 0;
-window.requestAnimationFrame(loop);
-
-const keyMap = {
-    'KeyW': 'up',
-    'KeyS': 'down',
-    'KeyA': 'left',
-    'KeyD': 'right',
-};
 
 function keydown(event) {
     const direction = keyMap[event.code];
@@ -80,5 +56,85 @@ function keyup(event) {
     input[direction] = false;
 }
 
-window.addEventListener("keydown", keydown)
-window.addEventListener("keyup", keyup)
+function resize(event) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function follow(follower, followee) {
+    follower.position.x = followee.position.x;
+    follower.position.y = followee.position.y;
+}
+
+////////////////////////////////////////////////////
+
+const camera = {
+    position: {
+        x: 0,
+        y: 0,
+    },
+    velocity: {
+        x: 0,
+        y: 0,
+    },
+};
+
+const player = {
+    width: 50,
+    height: 50,
+    speed: 100,
+    color: 'red',
+    position: {
+        x: 0,
+        y: 0,
+    },
+    velocity: {
+        x: 0,
+        y: 0,
+    }
+};
+
+const wall = {
+    width: 100,
+    height: 100,
+    color: 'white',
+    position: {
+        x: 0,
+        y: 0,
+    },
+    velocity: {
+        x: 0,
+        y: 0,
+    }
+};
+
+player.position.x = window.innerWidth / 2 - player.width / 2;
+player.position.y = window.innerHeight / 2 - player.height / 2;
+
+follow(camera, player);
+
+const input = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+};
+
+const canvas = document.getElementsByTagName('canvas')[0];
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const context = canvas.getContext('2d');
+
+let lastRender = 0;
+window.requestAnimationFrame(loop);
+
+const keyMap = {
+    'KeyW': 'up',
+    'KeyS': 'down',
+    'KeyA': 'left',
+    'KeyD': 'right',
+};
+
+window.addEventListener('keydown', keydown);
+window.addEventListener('keyup', keyup);
+window.addEventListener('resize', resize);
